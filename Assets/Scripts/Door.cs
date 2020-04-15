7e1ef7c0b505a7e04c77b65dccda14537682d42f
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2020-04-14 21:42:11 -0500

    Met Milestone 3 Requirments

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : InteractiveObject
+{
+    [Tooltip("Check this box to lock the door")]
+    [SerializeField] private bool isLocked;
+
+    [Tooltip("The text that displays when the player looks at the door while it's locked.")]
+    [SerializeField] private string lockedDisplayText = "Locked";
+
+    [Tooltip("Play this audio clip when the player interacts with a locked door without owning the key.")]
+    [SerializeField] private AudioClip lockedAudioClip;
+
+    [Tooltip("Play this audio clip when the player opens the door.")]
+    [SerializeField] private AudioClip openAudioClip;
+
+    public override string DisplayText => isLocked ? lockedDisplayText : base.DisplayText;
+
+    private Animator animator;
+    private bool isOpen = false;
+    /// <summary>
+    /// Using a constructor here to initialize DisplayText in the editor.
+    /// </summary>
+    public Door()
+    {
+        displayText = nameof(Door);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void InteractWith()
+    {
+        if (!isOpen)
+        {
+            if (!isLocked)
+            {
+                audioSource.clip = openAudioClip;
+                animator.SetBool("shouldOpen", true);
+                displayText = string.Empty;
+                isOpen = true;
+            }
+            else // If the door is locked
+            {
+                audioSource.clip = lockedAudioClip;
+            }
+            base.InteractWith(); // Play lock sound
+        }
+    }
+}
